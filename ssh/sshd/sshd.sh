f71eb4b93c05b76e27a6f2ac313d0f74c372c033
Marcel Maatkamp <m.maatkamp@gmail.com>
2016-01-06 17:18:00 +0100

    ssh

--- ssh/sshd/sshd.sh
+++ ssh/sshd/sshd.sh
@@ -2,13 +2,5 @@
 
 mkdir -p /var/run/sshd
 
-# prepare config file for key based auth
-sed -i "s/UsePrivilegeSeparation.*/UsePrivilegeSeparation no/g" /etc/ssh/sshd_config
-sed -i "s/UsePAM.*/UsePAM no/g" /etc/ssh/sshd_config
-sed -i "s/PermitRootLogin.*/PermitRootLogin yes/g" /etc/ssh/sshd_config
-sed -i "s/#AuthorizedKeysFile/AuthorizedKeysFile/g" /etc/ssh/sshd_config
-
-# start ssh daemon
-# /usr/sbin/sshd -D
 ssh-keygen -A 
-/usr/sbin/sshd -D
+/usr/sbin/sshd -D -dd
