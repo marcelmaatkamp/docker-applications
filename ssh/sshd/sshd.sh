952aa9db8370e8c6ba2812d391fdf3605066dfd5
Marcel Maatkamp <m.maatkamp@gmail.com>
2016-01-06 16:14:23 +0100

    ssh

--- ssh/sshd/sshd.sh
+++ ssh/sshd/sshd.sh
@@ -1,14 +1,5 @@
 #!/bin/sh
 
-# generate fresh rsa key
-ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
-
-# generate fresh dsa key
-ssh-keygen -f /etc/ssh/ssh_host_dsa_key -N '' -t dsa
-
-wget -qO- https://api.github.com/users/marcelmaatkamp/keys | grep "\"key\"\: \"" | sed -e 's/    "key\"\: \"//g' > /etc/ssh/authorized_keys/github
-
-#prepare run dir
 mkdir -p /var/run/sshd
 
 # prepare config file for key based auth
@@ -18,4 +9,6 @@ sed -i "s/PermitRootLogin.*/PermitRootLogin yes/g" /etc/ssh/sshd_config
 sed -i "s/#AuthorizedKeysFile/AuthorizedKeysFile/g" /etc/ssh/sshd_config
 
 # start ssh daemon
+# /usr/sbin/sshd -D
+ssh-keygen -A 
 /usr/sbin/sshd -D
