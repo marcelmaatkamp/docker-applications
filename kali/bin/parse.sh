5eb9db57c72ad2a9cdb8663306ca32925fdf3e89
marcel <m.maatkamp@gmail.com>
2016-06-02 14:15:02 +0200

    mac as routing key

--- kali/bin/parse.sh
+++ kali/bin/parse.sh
@@ -9,22 +9,25 @@ do
  wlan_mgt_ssid=$(echo $line | cut -d, -f3)
  radiotap_dbm_antsignal=$(echo $line | cut -d, -f4)
 
+ routing_key=$(echo $wlan_sa | sed -e 's/:/./g')
+  
  date_epoch=$(date +%s)
 
- echo -n "{ "
- echo -n " \"wifi\" : {" 
- echo -n " \"date_epoch\": $date_epoch," 
- echo -n " \"location\" : {"
- echo -n "   \"name\" : \"$LOCATION_NAME\", "
- echo -n "   \"lon\" : $LOCATION_LAT, "
- echo -n "   \"lat\" : $LOCATION_LON "
- echo -n "  },"
- echo -n " \"probe_request\": { "
- echo -n "   \"wlan.sa\" : \"$wlan_sa\", "
- echo -n "   \"wlan.sa_resolved\" : \"$wlan_sa_resolved\", "
- echo -n "   \"wlan_mgt.ssid\" : \"$wlan_mgt_ssid\", "
- echo -n "   \"radiotap.dbm_antsignal\" : $radiotap_dbm_antsignal "
- echo -n "  }"
- echo -n " }"
- echo "}"
+ json="{ "
+ json+=" \"wifi\" : {" 
+ json+=" \"date_epoch\": $date_epoch," 
+ json+=" \"location\" : {"
+ json+="   \"name\" : \"$LOCATION_NAME\", "
+ json+="   \"lon\" : $LOCATION_LAT, "
+ json+="   \"lat\" : $LOCATION_LON "
+ json+="  },"
+ json+=" \"probe_request\": { "
+ json+="   \"wlan.sa\" : \"$wlan_sa\", "
+ json+="   \"wlan.sa_resolved\" : \"$wlan_sa_resolved\", "
+ json+="   \"wlan_mgt.ssid\" : \"$wlan_mgt_ssid\", "
+ json+="   \"radiotap.dbm_antsignal\" : $radiotap_dbm_antsignal "
+ json+="  }"
+ json+=" }"
+ json+="}"  
+ echo "$json" | amqp-publish -u "amqp://$AMQP_USERNAME:$AMQP_PASSWORD@$AMQP_HOSTNAME" -r "$routing_key" -e wifi.probes -l
 done < "${1:-/dev/stdin}"
--- kali/bin/start.sh
+++ kali/bin/start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./collect.sh 2> /dev/null | ./parse.sh | amqp-publish -u amqp://wifi.probes:wifi.probes@stein.pirod.nl -e wifi.probes -l
+./collect.sh 2> /dev/null | ./parse.sh 
