67b10b453037174e4a6d08b5f7c57267d805a4e5
Marcel Maatkamp <m.maatkamp@gmail.com>
2016-06-09 15:23:52 +0200

    Delete collect_probes.sh

--- wifi/bin/collect_probes.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-device=$LOCATION_DEVICE
-iwconfig $device mode monitor
-tshark -l -i $device -T fields -e wlan.sa -e wlan.sa_resolved -e wlan_mgt.ssid -e radiotap.dbm_antsignal -E separator=, -I subtype probereq
