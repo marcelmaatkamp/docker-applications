36689ab939a905eb0de08f8e36e6d9f1d80471a5
Marcel Maatkamp <m.maatkamp@gmail.com>
2016-06-09 15:23:25 +0200

    Delete all.js

--- wifi/bin/all.js
+++
@@ -1,6 +0,0 @@
-var pcap=require('./node_pcap/pcap');
-pcap.createSession(process.env.LOCATION_DEVICE, '').
-  on('packet', function (raw_packet) {
-    console.log(JSON.stringify(pcap.decode.packet(raw_packet)));
-  }
-);
