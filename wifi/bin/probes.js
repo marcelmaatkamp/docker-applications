d8e240fd9713223202b69ba96ac7a6b290890882
Marcel Maatkamp <m.maatkamp@gmail.com>
2016-06-09 15:24:06 +0200

    Delete probes.js

--- wifi/bin/probes.js
+++
@@ -1,9 +0,0 @@
-var pcap=require('./node_pcap/pcap');
-pcap.createSession(process.env.LOCATION_DEVICE, '(type mgt) and (type mgt subtype probe-req )').
-        on('packet', function (raw_packet) {
-                console.log(JSON.stringify(pcap.decode.packet(raw_packet)));
-                with(pcap.decode.packet(raw_packet).payload.ieee802_11Frame)
-                        if (type == 0 && subType == 4)
-                                console.log("Probe request",shost, "-> ",bssid);
-        }
-);
