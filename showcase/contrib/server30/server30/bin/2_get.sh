284ad667d0cbcc220fbaf353e545abffdc2f50ff
Marcel Maatkamp <m.maatkamp@gmail.com>
2016-09-28 14:01:16 +0200

    kpn lora added

---
+++ showcase/contrib/server30/server30/bin/2_get.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+wget "https://support.comodo.com/index.php?/Knowledgebase/Article/GetAttachment/979/1056459" -O comodo/comodo-rsa-domain-validation-sha-2-w-root.ca-bundle
---
+++ showcase/contrib/server30/server30/bin/4_install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "copy :"
+echo " - sudo cp ./contrib/server30/server30/domain.key ./volumes/https/keys/domain.key"
+echo " - sudo cp ./contrib/server30/server30/comodo/chained.pem ./volumes/https/keys/chained.pem"
