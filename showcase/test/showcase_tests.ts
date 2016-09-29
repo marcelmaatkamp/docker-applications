9723049e881aa0ebdfffe27f8c1dc3c4f696e1ee
abreits <abreits@gmail.com>
2016-09-29 07:01:35 +0200

    showcase test docker basis image toegevoegd

---
+++ showcase/test/showcase_tests.ts
@@ -0,0 +1,11 @@
+/*
+ * SHOWCASE-TESTS
+ *
+ * 2016-09-29 Ab Reitsma
+ *
+ * Programma dat test of de invoer via RabbitMQ voor ShowCase
+ * overeenkomt met de verwachte uitvoer
+ *
+ */
+
+// TODO: test framework ontwerpen en bouwen
