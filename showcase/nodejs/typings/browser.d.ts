e7cbadc208db82498e0e9019663c6f6073f85536
abreits <abreits@gmail.com>
2016-10-11 07:08:13 +0200

    added nodejs volume preparations

---
+++ showcase/nodejs/typings/browser.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="browser/ambient/node/index.d.ts" />
+/// <reference path="browser/ambient/when/index.d.ts" />
+/// <reference path="browser/definitions/bluebird/index.d.ts" />
+/// <reference path="browser/definitions/chai/index.d.ts" />
