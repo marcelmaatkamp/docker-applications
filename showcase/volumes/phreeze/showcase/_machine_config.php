6b0cfeb9577798f9c4dcd25c162f1ce2af755836
r.kuipers@pirod.nl <r.kuipers@pirod.nl>
2016-10-27 09:00:40 +0200

    fixxed config2

--- showcase/volumes/phreeze/showcase/_machine_config.php
+++ showcase/volumes/phreeze/showcase/_machine_config.php
@@ -16,7 +16,7 @@ require_once("verysimple/HTTP/RequestUtil.php");
 
 /** database connection settings */
 GlobalConfig::$CONNECTION_SETTING = new ConnectionSetting();
-GlobalConfig::$CONNECTION_SETTING->ConnectionString = "mysql:3307";
+GlobalConfig::$CONNECTION_SETTING->ConnectionString = "mysql:3306";
 GlobalConfig::$CONNECTION_SETTING->Host = "mysql";
 GlobalConfig::$CONNECTION_SETTING->Port = "3306";
 GlobalConfig::$CONNECTION_SETTING->DBName = "showcase";
